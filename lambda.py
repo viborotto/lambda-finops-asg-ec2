2c7022d671539425c1111f48616e1ff599094d7d
viborotto <vittoriaborotto@gmail.com>
2022-02-02 23:22:49 -0300

    Lambda - Start & Stop

--- lambda.py
+++ lambda.py
@@ -1,36 +1,38 @@
 import boto3
+import os
+import json
 
 asg_client = boto3.client('autoscaling')
 
 filter_asg_list_names = asg_client.get_paginator('describe_auto_scaling_groups').paginate(
     PaginationConfig={'PageSize': 100}).search('AutoScalingGroups[].[AutoScalingGroupName]')
 
-tuple_start_desired = tuple(
-    asg_client.get_paginator('describe_auto_scaling_groups').paginate(PaginationConfig={'PageSize': 100}).search(
-        'AutoScalingGroups[].[DesiredCapacity]'))
+list_start_desired = list(
+    asg_client.get_paginator('describe_auto_scaling_groups').paginate(PaginationConfig={'PageSize': 100})
+        .search('AutoScalingGroups[].[Tags[?Key ==`{}`].Value[]]'
+        .format('Desired')))
 
-tuple_start_min = tuple(
-    asg_client.get_paginator('describe_auto_scaling_groups').paginate(PaginationConfig={'PageSize': 100}).search(
-        'AutoScalingGroups[].[MinSize]'))
+list_start_min = list(
+    asg_client.get_paginator('describe_auto_scaling_groups').paginate(PaginationConfig={'PageSize': 100})
+        .search('AutoScalingGroups[].[Tags[?Key ==`{}`].Value[]]'
+        .format('Minimum')))
 
 
 def lambda_handler(event, context):
     global cloudwatchvalue
-    cloudwatchvalue = "stop"
+    cloudwatchvalue = event.get('action')
 
-    for asg_item, desired, minimum in zip(filter_asg_list_names, tuple_start_desired, tuple_start_min):
+    for asg_item, desired, minimum in zip(filter_asg_list_names, list_start_desired, list_start_min):
         if 'start' == cloudwatchvalue:
-            print("starting scale out instances")
+            print("####### starting scale out instances #######")
 
-            print("ASG: {asg_name} set desired: {desired_count}"
-                  .format(asg_name=asg_item[0], desired_count=desired[0]))
-
-            update_asg_desired_count(asg_item[0], desired[0], minimum[0])
+            print("ASG: {asg_name}; Desired: {desired_count}; Minimum: {minimum_size}"
+                  .format(asg_name=asg_item[0], desired_count=desired[0][0], minimum_size=minimum[0][0]))
+            update_asg_desired_count(asg_item[0], desired[0][0], minimum[0][0])
 
         elif 'stop' == cloudwatchvalue:
-            print("stopping instances - scale in")
-            print("ASG: {asg_name} set desired: 0"
-                  .format(asg_name=asg_item[0]))
+            print("####### stopping instances - scale in ########")
+            print("ASG: {asg_name} set desired: 0".format(asg_name=asg_item[0]))
             update_asg_desired_count(asg_item[0], 0, 0)
 
 
@@ -38,6 +40,6 @@ def update_asg_desired_count(asg, new_desired_count, minimum):
     # para cada instancia atualizar o valor de desired
     asg_client.update_auto_scaling_group(
         AutoScalingGroupName=asg,
-        DesiredCapacity=new_desired_count,
-        MinSize=minimum,
+        DesiredCapacity=int(new_desired_count),
+        MinSize=int(minimum),
     )
